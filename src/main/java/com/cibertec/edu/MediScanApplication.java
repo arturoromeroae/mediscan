b149e24f92bcacd395e7583e260c0e0027b1a8f4
User <user@Mac-mini-de-User.local>
2023-10-12 17:43:21 -0500

    first commit

---
+++ src/main/java/com/cibertec/edu/MediScanApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediScanApplication.class, args);
+	}
+
+}
