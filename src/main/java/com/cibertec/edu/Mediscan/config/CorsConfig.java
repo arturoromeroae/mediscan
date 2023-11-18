54650edad8fe624299d332c46e86a4846ae5bf62
BryanBlas325 <brayananthony129@gmail.com>
2023-11-17 19:35:03 -0500

    Cors Update

--- src/main/java/com/cibertec/edu/Mediscan/config/CorsConfig.java
+++ src/main/java/com/cibertec/edu/Mediscan/config/CorsConfig.java
@@ -1,24 +1,26 @@
 package com.cibertec.edu.Mediscan.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.*;
 
 @Configuration
 public class CorsConfig {
 	@Bean
-	public CorsFilter filter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		
-		config.addAllowedOrigin("*");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", config);
-        
-        return new CorsFilter(source);
-	};
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+        		"https://mediscan-99f31c760e05.herokuapp.com",
+        		"https://mediscan-99f31c760e05.herokuapp.com/api/usuario",
+        		"https://mediscan-99f31c760e05.herokuapp.com/api/bot"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
