25d57d59671e52807833d9b9cac00b29508ba1b5
BryanBlas325 <brayananthony129@gmail.com>
2023-11-17 19:09:26 -0500

    Configuracion CORS

---
+++ src/main/java/com/cibertec/edu/Mediscan/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.cibertec.edu.Mediscan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	@Bean
+	public CorsFilter filter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		
+		config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        
+        return new CorsFilter(source);
+	};
+}
